5490ab3c1f9ac7374ad6f9b6b72f0506d187a603
Jan Eglinger <jan.eglinger@fmi.ch>
2018-07-12 13:28:00 +0200

    FileBatchService: add check for compatible inputs

--- src/main/java/org/scijava/batch/FileBatchService.java
+++ src/main/java/org/scijava/batch/FileBatchService.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 
 import org.scijava.Priority;
 import org.scijava.command.CommandService;
+import org.scijava.log.LogService;
 import org.scijava.module.ModuleInfo;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
@@ -15,6 +16,9 @@ import org.scijava.service.Service;
 public final class FileBatchService extends AbstractService implements
 		BatchService {
 
+	@Parameter
+	private LogService log;
+	
 	@Parameter
 	private CommandService commandService;
 
@@ -28,6 +32,11 @@ public final class FileBatchService extends AbstractService implements
 
 	@Override
 	public void run(ModuleInfo moduleInfo) {
+		// check if moduleInfo has batchable inputs
+		if (batchableInputs(moduleInfo).isEmpty()) {
+			log.error("No compatible inputs (of type File) found.");
+			return;
+		}
 		// Call ModuleBatchProcessor with input moduleInfo
 		HashMap<String, Object> inputMap = new HashMap<>();
 		inputMap.put("moduleInfo", moduleInfo);
